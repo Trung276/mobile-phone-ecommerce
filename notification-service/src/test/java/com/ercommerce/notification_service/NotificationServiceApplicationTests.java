e557593e107201b00a6583f15631f6abee7ee2ee
Trung276 <thanhtrung27061996@gmail.com>
2024-09-17 11:55:53 +0700

    create mobile phone e-commerce project

---
+++ notification-service/src/test/java/com/ercommerce/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ercommerce.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
